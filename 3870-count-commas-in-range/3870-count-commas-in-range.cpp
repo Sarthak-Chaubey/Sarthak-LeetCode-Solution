69bf3a34836b447fa6ec08d392eeb286ced60c1e
Sarthak Chaubey <149096318+Sarthak-Chaubey@users.noreply.github.com>
2026-09-08 15:04:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int c = 0;
-        for(int i = 1; i<=n; i++){
-            if(i>=1000)
-            c++;
-        }
-        return c;
+        // int c = 0;
+        // for(int i = 1; i<=n; i++){
+        //     if(i>=1000)
+        //     c++;
+        // }
+        // return c;
+
+        if(n < 1000) return 0;
+        return n - 999;
     }
         
 };
