0fa05ad2dd090e50be2615d17a1b2c23c7ff4438
Sarthak Chaubey <149096318+Sarthak-Chaubey@users.noreply.github.com>
2026-09-23 14:48:51 +0530

    Time: 2 ms (49.8%), Space: 263.4 MB (22.76%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rev(vector<int>& nums,int left, int right){
+        while(left<right)
+        {
+            swap(nums[left],nums[right]);
+                left++;
+                right--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n= nums.size();
+        k = k%n;
+        rev(nums,0,n-1);
+        rev(nums,0,k-1);
+        rev(nums,k,n-1);
+    }
+};
